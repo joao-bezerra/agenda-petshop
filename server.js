59ef7b655d4993c7c8f2946e09afbacff307b52b
João Leite <joao.leite@restoque.com.br>
2020-05-28 19:33:03 -0300

    Projeto agenda-petshop

---
+++ server.js
@@ -0,0 +1,27 @@
+const {GraphQLServer} = require ('graphql-yoga')
+const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/database/tabelas')
+
+
+
+conexao.connect(erro => {
+  if (erro) {
+    console.log(erro)
+  }
+
+  console.log('conectou no banco')
+
+  Tabelas.init(conexao)
+})
+
+const resolvers = {
+  Query: {
+    status: () => "servidor rodando!"
+  }
+}
+const servidor = new GraphQLServer({
+  resolvers,
+  typeDefs: './schema.graphql'
+})
+
+servidor.start(()=> console.log('servidor ouvindo'))
